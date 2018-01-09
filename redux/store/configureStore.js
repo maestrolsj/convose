2437b6bdd7cb6d03f4fc21f06a6e8166130b8928
maestro <maestrollsj@gmail.com>
2018-01-09 17:58:09 +0100

    add middleware for People List

--- redux/store/configureStore.js
+++ redux/store/configureStore.js
@@ -3,13 +3,13 @@ import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import promise from "redux-promise"
 import logger from "redux-logger"
-import { auth, storage } from "../middleware"
+import { auth, storage,people } from "../middleware"
 import reducers from "../reducers"
 
 export default function configureStore(initialState = {}) {
  // const enhancers = [applyMiddleware(thunk, promise, logger, auth, storage)]
   //return createStore(reducers, initialState, compose(...enhancers))
 
-  return applyMiddleware(thunk, promise, logger, auth, storage)(createStore)(reducers, initialState)
+  return applyMiddleware(thunk, promise, logger, auth, storage,people)(createStore)(reducers, initialState)
 
 }
