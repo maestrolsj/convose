c452cd142f2e472dc2a54b88217395da9eaec667
maestro <maestrollsj@gmail.com>
2018-01-16 17:41:31 +0100

    Connect Profile View with Redux in order to show user informaion.

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Animated, Dimensions, Button,TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
+import {connect}            from 'react-redux'
 const DeviceWidth = Dimensions.get('window').width;
 const DeviceHeight = Dimensions.get('window').height;
 
@@ -9,14 +9,33 @@ const styles = StyleSheet.create({
   container: {position:'absolute', width:DeviceWidth, height:DeviceHeight, backgroundColor:'transparent'},
 });
 
-const Profile = ({ children }) => (
-  <TouchableHighlight  style={styles.container} onPress={Actions.pop} underlayColor="transparent" activeOpacity={1}>
 
-     <View style={{position:'absolute',top:0,right:0,backgroundColor:'white', width:DeviceWidth-80,height:DeviceHeight}}>
-        <Text style={{marginTop:100}}>Profile</Text>
-       <Text>{this.props.userInfo.email}</Text>
-      </View>
-  </TouchableHighlight>
-);
+class Profile extends React.Component {
 
-export default Profile;
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableHighlight  style={styles.container} onPress={Actions.pop} underlayColor="transparent" activeOpacity={1}>
+
+        <View style={{position:'absolute',top:0,right:0,backgroundColor:'white', width:DeviceWidth-80,height:DeviceHeight}}>
+          <Text style={{marginTop:100}}>Profile</Text>
+          <Text>{this.props.userInfo.email}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+
+
+const mapStateToProps = (state) => ({
+  userInfo: state.storage.userInfo,
+
+});
+
+
+
+export default connect(mapStateToProps)(Profile)
