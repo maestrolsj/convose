37e5f2eac3958cfea31f2ba89109ec2c6cea7306
maestro <maestrollsj@gmail.com>
2018-01-29 16:14:48 +0100

    Added Api  - Chat     : Fetch to conversation  - Sign up : Register new User info

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
-import { checkStatus } from './utils'
+import {checkStatus} from './utils'
 import * as ENDPOINTS from './endpoints'
 
 /**
  * Global Fetch
  */
-const fetchEndpoint = ({ endpoint, id, values, method = 'POST', token }) => {
+const fetchEndpoint = ({endpoint, id, values, method = 'POST', token}) => {
   console.log('fetching endpoint: ', `${endpoint}${id ? `/${id}` : '' }`, method, id, values, token)
   return fetch(`${endpoint}${id ? `/${id}` : '' }`, {
     method,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Authorization' : token
+      'Authorization': token
     },
     body: JSON.stringify(values)
   })
-  .then(checkStatus)
-  .then(response =>   response.json()  )
+    .then(checkStatus)
+    .then(response => response.json())
 }
 
 
@@ -25,9 +25,18 @@ const fetchEndpoint = ({ endpoint, id, values, method = 'POST', token }) => {
  */
 export const fetchLogin = values => {
   const endpoint = ENDPOINTS.LOGIN
-  return fetchEndpoint({ endpoint, values })
+  return fetchEndpoint({endpoint, values})
 }
 
+export const registerUser = v => {
+  const endpoint = ENDPOINTS.REGISTER_USER
+  const method   = "PUT";
+  const token    = v.token;
+  const values   = v.data; // userInfo
+
+  return fetchEndpoint({endpoint, method, token, values});
+
+}
 /**
  * Mock fetch, in case you need it
  */
@@ -40,11 +49,21 @@ export const mockFetch = () => {
 }
 
 
+// Fetch Chating Conversation Data from DB
+export const fetchChatContent = values => {
+
+  const endpoint = `${ENDPOINTS.FETCH_CONVERSATION}${values.userInfo.uuid}xx-xx${values.partnerInfo.uuid}.json?from=0&limit=10&p=${values.partnerInfo.uuid}=${values.userInfo.uuid}`;
+  const method = 'GET';
+  const token = values.userInfo.authentication_token;
+
+  return fetchEndpoint({endpoint, method, token});
+}
+
 export const fetchSuggestion = values => {
 
-  const  endpoint = ENDPOINTS.SUGGESTION + values;
-  const  method   = 'GET';
-  return fetchEndpoint({ endpoint, method });
+  const endpoint = ENDPOINTS.SUGGESTION + values;
+  const method = 'GET';
+  return fetchEndpoint({endpoint, method});
 
 }
 
@@ -52,15 +71,15 @@ export const fetchPeople = values => {
 
   const endpoint = ENDPOINTS.SEARCH_PEOPLE + `?page=0&limit=6`;
   const method = 'GET';
-  const token  = values;
+  const token = values;
 
-  return fetchEndpoint({ endpoint, method, token });
+  return fetchEndpoint({endpoint, method, token});
 }
 
 export const fetchGuestInfo = () => {
 
-  const  endpoint = ENDPOINTS.GUESTINFO;
-  const  method   = 'POST';
-  return fetchEndpoint({ endpoint, method });
+  const endpoint = ENDPOINTS.GUESTINFO;
+  const method = 'POST';
+  return fetchEndpoint({endpoint, method});
 }
 
