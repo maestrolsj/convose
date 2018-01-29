9c9927dc1b85316ac2a0473d254b9e7887bd23f7
maestro <maestrollsj@gmail.com>
2018-01-29 16:09:44 +0100

    [ Added Actions ]
     -  UPDATE_PARTNERINFO
     -  GET_CHAT_CONTENT
     -  GET_CHAR_ROOMINFO
     -  REGISTER_USER
     -  REGISTER_USER_ERROR
     -  GET_USER_INFO

--- redux/actions/actionTypes.js
+++ redux/actions/actionTypes.js
@@ -1,24 +1,33 @@
 export default ACTION_TYPES = {
-    FETCH_IN_PROGRESS        : "FETCH_IN_PROGRESS",
-    FETCH_FAILURE            : "FETCH_FAILURE",
-    FETCH_SUCCESS            : "FETCH_SUCCESS",
-    LOGIN_SUCCESS            : "LOGIN_SUCCESS",
-    LOGOUT_SUCCESS           : "LOGOUT_SUCCESS",
+    FETCH_IN_PROGRESS        : "FETCH_IN_PROGRESS"       ,
+    FETCH_FAILURE            : "FETCH_FAILURE"           ,
+    FETCH_SUCCESS            : "FETCH_SUCCESS"           ,
+    LOGIN_SUCCESS            : "LOGIN_SUCCESS"           ,
+    LOGOUT_SUCCESS           : "LOGOUT_SUCCESS"          ,
 
-    GET_AUTH_STORAGE         : "GET_AUTH_STORAGE",
-    SAVE_AUTH_STORAGE        : "SAVE_AUTH_STORAGE",
+    GET_AUTH_STORAGE         : "GET_AUTH_STORAGE"        ,
+    SAVE_AUTH_STORAGE        : "SAVE_AUTH_STORAGE"       ,
     GET_AUTH_STORAGE_SUCCESS : "GET_AUTH_STORAGE_SUCCESS",
     GET_AUTH_STORAGE_FAILURE : "GET_AUTH_STORAGE_FAILURE",
-    CHANGE_PASSWORD_SUCCESS  : "CHANGE_PASSWORD_SUCCESS",
-    ERROR                    : "ERROR",
+    CHANGE_PASSWORD_SUCCESS  : "CHANGE_PASSWORD_SUCCESS" ,
+    ERROR                    : "ERROR"                   ,
 
-    FETCH_SUGGESTIONS        : "FETCH_SUGGESTIONS",
-    CHANGE_SUGGESTIONS       : "CHANGE_SUGGESTIONS",
+    FETCH_SUGGESTIONS        : "FETCH_SUGGESTIONS"       ,
+    CHANGE_SUGGESTIONS       : "CHANGE_SUGGESTIONS"      ,
 
-    CANCEL_FETCHING          : "CANCEL_FETCHING",
-    CLEAR_SUGGESTIONS        : "CLEAR_SUGGESTIONS",
-    UPDATE_PROFILE           : "UPDATE_PROFILE",
+    CANCEL_FETCHING          : "CANCEL_FETCHING"         ,
+    CLEAR_SUGGESTIONS        : "CLEAR_SUGGESTIONS"       ,
+    UPDATE_PROFILE           : "UPDATE_PROFILE"          ,
 
-    SEARCH_PEOPLE            : "SEARCH_PEOPLE"
+    SEARCH_PEOPLE            : "SEARCH_PEOPLE"           ,
+
+    UPDATE_PARTNERINFO       : "UPDATE_PARTNERINFO"      ,
+    GET_CHAT_CONTENT         : "GET_CHAT_CONTENT"        ,
+    GET_CHAR_ROOMINFO        : "GET_CHAR_ROOMINFO"       ,
+
+    REGISTER_USER            : "REGISTER_USER"           ,
+    REGISTER_USER_ERROR      : "REGISTER_USER_ERROR"     ,
+
+    GET_USER_INFO            : "GET_USER_INFO"           ,
 
 }
