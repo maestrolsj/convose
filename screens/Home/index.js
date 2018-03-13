6e4547cf074e0df454ae8242f1e0eecd4f14e210
maestro <maestrollsj@gmail.com>
2018-03-13 22:53:24 +0100

    Separate the Wrapper from Component.
    Wrapper is in index file

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,3 +1,16 @@
+import {connect} from 'react-redux'
+import {getStorage} from "../../redux/reducers"
 import Home from "./Home"
+import * as actions from './../../redux/actions'
 
-export default Home
+
+
+const mapStateToProps = (state, props) => ({
+  ...getStorage(state),
+})
+
+const mapDispatchToProps = {
+  ...actions,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
