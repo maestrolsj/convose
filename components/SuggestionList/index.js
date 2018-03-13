0a0ba012d2d346dc1357f1dc8c5ba2660d40db8e
maestro <maestrollsj@gmail.com>
2018-03-13 22:45:31 +0100

    Separate the Wrapper from Component.
    Wrapper is in index file

--- components/SuggestionList/index.js
+++ components/SuggestionList/index.js
@@ -1,3 +1,18 @@
+import {connect} from 'react-redux'
+import {getStorage,getSuggestedWords,getNetwork} from "../../redux/reducers"
 import SuggestionList from "./SuggestionList"
+import * as actions from './../../redux/actions'
 
-export default SuggestionList
+
+
+const mapStateToProps = (state, props) => ({
+  ...getStorage(state),
+  ...getSuggestedWords(state),
+  ...getNetwork(state)
+})
+
+const mapDispatchToProps = {
+  ...actions,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SuggestionList)
