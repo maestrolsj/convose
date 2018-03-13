fae1604d535db31df7a0707b2cc520d8cba77f3b
maestro <maestrollsj@gmail.com>
2018-03-13 22:49:48 +0100

    organize the reducer for suggested ppl list * didn't modify much . nothing special

--- redux/reducers/suggest.js
+++ redux/reducers/suggest.js
@@ -1,39 +1,24 @@
 import ACTION_TYPES from "../actions/actionTypes"
 
-
-
-
-export const fetching = (state=false, action) => {
-
-  switch(action.type) {
-
-    case ACTION_TYPES.FETCH_SUGGESTIONS :
-      console.log("FETCH_SUGGESTIONS REDUCER");
-      return true
-
-    case ACTION_TYPES.CANCEL_FETCHING :
-      console.log("CANCEL_FETCHING REDUCER")
-      return false
-
-    case ACTION_TYPES.CHANGE_SUGGESTIONS :
-      console.log("CHANGE_SUGGESTIONS REDUCER");
-      return false
-
-    default:
-      return state
-  }
-
+const initialState = {
+  suggestedWords :[]
 }
 
-export const suggestions = (state=[], action) => {
+export default function(state=initialState, action)  {
 
   switch(action.type) {
 
     case ACTION_TYPES.CLEAR_SUGGESTIONS :
-      return []
+      return {
+        ...state,
+        suggestedWords:[]
+      }
 
     case ACTION_TYPES.CHANGE_SUGGESTIONS :
-      return action.payload
+      return {
+        ...state,
+        suggestedWords:action.payload.suggestedWords
+      }
 
     default :
       return state
